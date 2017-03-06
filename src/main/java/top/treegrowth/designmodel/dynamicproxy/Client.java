789b9c75451747858bd00b8cc3a7c42282fa89b2
wusi06 <wusi0109@163.com>
2017-03-06 13:19:13 +0800

    添加动态代理设计模式

---
+++ src/main/java/top/treegrowth/designmodel/dynamicproxy/Client.java
@@ -0,0 +1,19 @@
+package top.treegrowth.designmodel.dynamicproxy;
+
+/**
+ * @author wusi
+ * @version 2017/3/6 13:10
+ */
+public class Client {
+    public static void main(String[] args) {
+        Object result = null;
+        ISubject subject = new ConcreteSubject();
+        DynamicProxy dynamicProxy = new DynamicProxy(subject);
+        String[] stringParam = {"1111"};
+        try {
+            result = dynamicProxy.exec("doSomething", stringParam);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/top/treegrowth/designmodel/dynamicproxy/ConcreteSubject.java
@@ -0,0 +1,12 @@
+package top.treegrowth.designmodel.dynamicproxy;
+
+/**
+ * @author wusi
+ * @version 2017/3/6 12:49
+ */
+public class ConcreteSubject implements ISubject {
+    @Override
+    public void doSomething(String str) {
+        System.out.println("实际的方法" + str);
+    }
+}
