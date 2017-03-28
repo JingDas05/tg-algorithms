08aea9caa35c1d8b5a4c8f35aea6264cfa1e0673
wusi06 <wusi0109@163.com>
2017-03-28 09:00:25 +0800

    解读源码消息发布接受（阅读一部分）

--- src/main/java/top/treegrowth/javasource/publishervslistener/Client.java
+++ src/main/java/top/treegrowth/javasource/publishervslistener/Client.java
@@ -1,8 +1,17 @@
 package top.treegrowth.javasource.publishervslistener;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @author wusi
  * @version 2017/3/28 7:42.
  */
 public class Client {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(EventConfig.class);
+        DemoPublisher demoPublisher = (DemoPublisher)context.getBean("demoPublisher");
+        demoPublisher.publish("清晨的第一个消息");
+    }
 }
