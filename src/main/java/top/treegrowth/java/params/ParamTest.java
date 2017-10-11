4e408e8c13d44f6595126d2076738bdb0f816e8e
wusi06 <wusi0109@163.com>
2017-10-11 10:29:15 +0800

    测试值引用传递

--- src/main/java/top/treegrowth/java/params/ParamTest.java
+++ src/main/java/top/treegrowth/java/params/ParamTest.java
@@ -25,6 +25,12 @@ public class ParamTest {
         paramTest.testStringObjectD(d);
         System.out.println(d);
 
+        // 最终级别
+        StringBuffer e = new StringBuffer("e");
+        StringBuffer f = new StringBuffer("f");
+        paramTest.changeStringBufferEAndF(e, f);
+        System.out.println(e);
+        System.out.println(f);
     }
 
     // 这个地方的a是形参
@@ -46,5 +52,15 @@ public class ParamTest {
     public void testStringObjectD(String d) {
         d = new String("str_object_new");
         System.out.println(d);
+    }
+
+    // 传递了两个地址 e, f, 地址是按值传递的
+    public void changeStringBufferEAndF(StringBuffer e, StringBuffer f) {
+        // 这个地方会修改e所指向的字符串的值，因此会修改实参e的值
+        e.append("add");
+        // 这个地方居然没有改变f，蒙圈
+        // 执行这个地方的时候，是引用传递的 “值传递”，只会修改形参的地址，不会修改实参，也就是不会修改外面传入的f
+        f = e;
+        System.out.println(f);
     }
 }
