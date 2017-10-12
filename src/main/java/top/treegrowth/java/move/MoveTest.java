f5f8dc590e1ad4627845b64226fecf0459cbe5b6
wusi06 <wusi0109@163.com>
2017-10-12 17:51:52 +0800

    测试值引用传递

---
+++ src/main/java/top/treegrowth/java/move/MoveTest.java
@@ -0,0 +1,16 @@
+package top.treegrowth.java.move;
+
+/**
+ * @author wusi
+ * @version 2017/10/12 17:47
+ */
+public class MoveTest {
+
+    public static void main(String[] args) {
+        int i = -4;
+        System.out.println(i);
+        System.out.println(Integer.toBinaryString(i));
+
+
+    }
+}
