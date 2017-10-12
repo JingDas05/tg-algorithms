6990ffb4f191a280a811b9fd468e9aa6c33b89de
wusi05 <wusi05@126.com>
2017-10-13 07:19:09 +0800

    反射的一些例子

--- src/main/java/top/treegrowth/java/reflect/Client.java
+++ src/main/java/top/treegrowth/java/reflect/Client.java
@@ -13,9 +13,15 @@ public class Client {
         Method method = clazz.getMethod("method1", String.class);
         method.getDeclaringClass();
 
+        //getDeclaringClass 方法会返回当前方法所在类的class对象！！！！！
+
         Class<TargetInterface> clazzInterface = TargetInterface.class;
+        Class<ParentInterface> clazzParentInterface = ParentInterface.class;
         Method testMethod = clazzInterface.getMethod("testMethod");
+        Method testMethodParent = clazzInterface.getMethod("testMethodParent");
         System.out.println(testMethod.getDeclaringClass().equals(clazzInterface));
+        System.out.println(testMethodParent.getDeclaringClass().equals(clazzParentInterface));
+
         // clazzInterface.getInterfaces() 会获取clazzInterface的所有直接父接口
         for (Class<?> each : clazzInterface.getInterfaces()) {
             System.out.println(each);
--- src/main/java/top/treegrowth/java/reflect/ParentInterface.java
+++ src/main/java/top/treegrowth/java/reflect/ParentInterface.java
@@ -4,5 +4,7 @@ package top.treegrowth.java.reflect;
  * @author wusi
  * @version 2017/10/12 7:20.
  */
-public interface ParentInterface extends GrandInterface{
+public interface ParentInterface extends GrandInterface {
+
+    void testMethodParent();
 }
