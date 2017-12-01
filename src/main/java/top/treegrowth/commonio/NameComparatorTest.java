4a45b980b6858cf6bf08c33b2e449b1a79876bd6
wusi06 <wusi0109@163.com>
2017-12-01 14:47:04 +0800

    添加文件比较器

---
+++ src/main/java/top/treegrowth/commonio/NameComparatorTest.java
@@ -0,0 +1,25 @@
+package top.treegrowth.commonio;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.io.comparator.NameFileComparator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author wusi
+ * @version 2017/12/1 13:41
+ */
+public class NameComparatorTest {
+
+    public static void main(String[] args) {
+        List<File> files = Lists.newArrayList(new File("1.txt"), new File("3.txt"), new File("2.txt"));
+        ((NameFileComparator) NameFileComparator.NAME_COMPARATOR).sort(files);
+        Iterator<File> fileIterator = files.iterator();
+        while (fileIterator.hasNext()) {
+            System.out.println(fileIterator.next().getName());
+        }
+    }
+}
