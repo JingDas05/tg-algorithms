09ea53ea12992aba55ae4ae63a4735dc7b4523d5
wusi06 <wusi0109@163.com>
2017-12-04 17:03:40 +0800

    添加文件比较器

---
+++ src/main/java/top/treegrowth/algorithms/datastructure/linked/MyLinkedList.java
@@ -0,0 +1,13 @@
+package top.treegrowth.algorithms.datastructure.linked;
+
+/**
+ * @author wusi
+ * @version 2017/12/4 9:30
+ */
+public class MyLinkedList {
+
+    // 链表头的引用
+    Node head = null;
+
+
+}
---
+++ src/main/java/top/treegrowth/algorithms/datastructure/linked/Node.java
@@ -0,0 +1,15 @@
+package top.treegrowth.algorithms.datastructure.linked;
+
+/**
+ * @author wusi
+ * @version 2017/12/4 9:26
+ */
+public class Node {
+
+    Node next = null;
+    Object data;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+}
