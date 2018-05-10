3707177a1ddd182a7b0aad5e190dc3ffb323c59f
wusi <wusi@dianwoba.com>
2018-05-10 15:34:16 +0800

    添加double计算的用例测试

---
+++ src/main/java/top/treegrowth/java/doubletest/DoubleComputeTest.java
@@ -0,0 +1,12 @@
+package top.treegrowth.java.doubletest;
+
+/**
+ * @author Administrator
+ */
+public class DoubleComputeTest {
+
+    public static void main(String[] arg) {
+        Integer a = 39780000;
+        System.out.println(a / 1000000D);
+    }
+}
