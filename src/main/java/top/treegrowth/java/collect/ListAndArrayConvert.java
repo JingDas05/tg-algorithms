fe54adf9df2272af50f38ec87ed69608b28ead53
wusi <wusi@dianwoba.com>
2018-08-14 11:21:18 +0800

    添加数组和list相互转换的代码

---
+++ src/main/java/top/treegrowth/java/collect/ListAndArrayConvert.java
@@ -0,0 +1,20 @@
+package top.treegrowth.java.collect;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author wusi
+ * @version 2018/8/14.
+ */
+public class ListAndArrayConvert {
+
+    public static void main(String[] args) {
+        List<String> elements = new ArrayList<>();
+        elements.add("1");
+        String[] strings = elements.toArray(new String[0]);
+        elements.addAll(Arrays.asList("2", "3"));
+        System.out.println(elements);
+    }
+}
