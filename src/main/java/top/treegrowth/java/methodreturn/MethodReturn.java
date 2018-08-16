60802a11441209f71d5c8779800cb0c322c8a466
wusi <wusi@dianwoba.com>
2018-08-16 17:59:41 +0800

    添加配置自动注入demo

---
+++ src/main/java/top/treegrowth/java/methodreturn/MethodReturn.java
@@ -0,0 +1,17 @@
+package top.treegrowth.java.methodreturn;
+
+/**
+ * @author wusi
+ * @version 2018/8/15.
+ */
+public class MethodReturn {
+
+    public static void main(String[] args) {
+        returnMethod();
+        System.out.println("打印");
+    }
+
+    public static void returnMethod() {
+        return;
+    }
+}
