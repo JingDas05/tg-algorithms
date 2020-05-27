90df2fa6419666311d596b83ca14878792ff8acf
wusi <wusi>
2020-05-27 08:59:55 +0800

    令牌桶测试

--- src/main/java/top/treegrowth/guava/limiter/RateLimiterTest.java
+++ src/main/java/top/treegrowth/guava/limiter/RateLimiterTest.java
@@ -11,11 +11,12 @@ public class RateLimiterTest {
 
 
     public static void main(String[] args) throws InterruptedException {
-        RateLimiter rateLimiter = RateLimiter.create(0.1);
+        RateLimiter rateLimiter = RateLimiter.create(1);
         for (int i = 0; i < 5; i++) {
             //acquire方法传入的是需要的令牌个数，当令牌不足时会进行等待，该方法返回的是等待的时间
             double waitTime = rateLimiter.acquire(10);
             boolean result = rateLimiter.tryAcquire();
+            rateLimiter.setRate(2);
             System.out.println("waitTime," + waitTime);
             System.out.println("threadId gain result," + result);
             System.out.println("threadId," + Thread.currentThread().getId());
